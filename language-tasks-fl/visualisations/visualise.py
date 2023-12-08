6a5c2757c20b8d1280b2574753bf857591e7fa28
Jan <j2000.vdm@gmail.com>
2023-12-08 18:44:29 +0100

    Ran a bunch of tests and updated file structure.

--- language-tasks-fl/visualisations/visualise.py
+++ language-tasks-fl/visualisations/visualise.py
@@ -2,32 +2,40 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 # Read the stats.csv file
-data = pd.read_csv('../out/sent140-greek-krum-2-10-190-lr-adapt/stats.csv')
-
+data = pd.read_csv('../out/single-character-test/stats.csv')
+data2 = pd.read_csv('../out/sent140-greek-director-backdoor-2-10-190-lr-adapt/stats.csv')
 #### MAIN TASK ACCURACY
 # Extract the accuracy column
-accuracy = data['main_task_acc']
+# accuracy = data['main_task_acc']
+# baseline_accuracy = data2['main_task_acc']
 # Create a line plot of the accuracy
-plt.plot(accuracy)
+# plt.plot(accuracy)
+# plt.plot(baseline_accuracy)
 
 # Set the plot title and labels
-plt.title('Accuracy')
-plt.xlabel('Epoch')
-plt.ylabel('Accuracy')
+# plt.title('Accuracy')
+# plt.xlabel('Epoch')
+# plt.ylabel('Accuracy')
 
 # Create a line plot of the accuracy whilst averaging over 15 epochs
-# plt.plot(accuracy.rolling(15).mean())
-
+# plt.plot(accuracy.rolling(30).mean())
+# plt.plot(baseline_accuracy.rolling(30).mean())
+# plt.legend(['accuracy single character attack', 'accuracy baseline'])
 # Show the plot
 # plt.show()
 
 #### BACKDOOR ACCURACY
-# backdoor_acc = data['backdoor_acc']
-# plt.xlabel('Epoch')
-# plt.ylabel('Backdoor Accuracy')
-# plt.plot(backdoor_acc)
-# plt.title('Backdoor Accuracy with Krum Defense')
+backdoor_acc = data['backdoor_acc']
+plt.xlabel('Epoch')
+plt.ylabel('Backdoor Accuracy')
+plt.plot(backdoor_acc)
+plt.title('Backdoor Accuracy with No Defense')
+backdoor_acc2 = data2['backdoor_acc']
+plt.plot(backdoor_acc2)
+
+#Create a legend
+plt.legend(['accuracy single character attack', 'accuracy edge case attack'])
 
 # Save the visualization in the current folder
-plt.savefig('accuracy_greek_director_krum.png', dpi=300, bbox_inches='tight')
+plt.savefig('backdoor_acc_single_edge_nodefense.png', dpi=300, bbox_inches='tight')
 
