0091889c9e4b06b345015e008be438f455f95f81
Jan <j2000.vdm@gmail.com>
2023-12-18 16:56:34 +0100

    Run tests for non-iid case

--- language-tasks-fl/run_all.sh
+++ language-tasks-fl/run_all.sh
@@ -1,7 +1,2 @@
-python fl_runner.py --conf fl-configs/attack-strategies/epoch1.yaml
-python fl_runner.py --conf fl-configs/attack-strategies/epoch200.yaml
-python fl_runner.py --conf fl-configs/attack-strategies/epoch400.yaml
-python fl_runner.py --conf fl-configs/attack-strategies/epoch500.yaml
-python fl_runner.py --conf fl-configs/robustness/robustness_fully_trained.yaml
 python fl_runner.py --conf fl-configs/non-iid/single-character-krum-natural.yaml
 python fl_runner.py --conf fl-configs/non-iid/single-character-krum-natural2.yaml
